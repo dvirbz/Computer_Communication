cf54c28f08a34ca0da2de369b1e6a5528977ff43
AmitZUlan <zulan@mail.tau.ac.il>
2021-03-20 17:31:10 +0200

    Create Defines.h

---
+++ Ex1/Defines.h
@@ -0,0 +1,36 @@
+/* Owners: Dvir Ben Zikri
+		   Amit Zulan     */
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+   // includes //
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "tchar.h"
+#include "math.h"
+#include "winsock2.h"
+#include "ws2tcpip.h"
+#include "io.h"
+#pragma warning(disable: 6387)
+
+typedef char* string;
+
+#define NUM_OF_ARGC_SNDR 4
+#define NUM_OF_ARGC_RCVR 3
+#define MAX_IP_LEN 15
+#define BUFFER_SIZE_SNDR 12
+#define STARTUP_FAIL "WSAStartup() Failed with Error: %d\n"
+#define SUCCESSCODE 0
+#define ERRORCODE -1
+#define LOCALHOST "127.0.0.1"
+#define STDIN 0
+#define END "End"
+#define BUFFER_SIZE_RCVR 17
+#define BYTESIZE 8
+#define DATASIZE (BUFFER_SIZE_SNDR - 1)
+
+
+#endif	// DEFINES_H
